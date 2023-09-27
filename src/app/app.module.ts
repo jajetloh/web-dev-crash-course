ef72bf08981f7bac262bc098287dd4b1125f624e
jajet.loh <jajetloh@gmail.com>
2023-09-27 19:08:43 +0800

    partial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AppComponent } from "./app.component"
+import { AppRoutingModule } from "./app-routing.module"
+import { HomeComponent } from "./home/home.component"
+import { ResourcesComponent } from "./resources/resources.component"
+import { TutorialComponent } from "./tutorial/tutorial.component"
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
 
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
+        ResourcesComponent,
+        TutorialComponent,
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
     ]
 })
 export class AppModule {
