225497c6c8996568277f7485889c35105f79e6d0
jajet.loh <jajetloh@gmail.com>
2023-09-27 18:58:54 +0800

    partial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,15 @@
 import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { CommonModule } from '@angular/common'
+import { AppComponent } from "./app.component"
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { TutorialComponent } from './tutorial/tutorial.component';
-import { HomeComponent } from './home/home.component';
-import { ResourcesComponent } from './resources/resources.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { FormsModule } from "@angular/forms"
 
 @NgModule({
     declarations: [
         AppComponent,
-        TutorialComponent,
-        HomeComponent,
-        ResourcesComponent
     ],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgbModule,
-        FormsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+        CommonModule
+    ]
 })
 export class AppModule {
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-/// <reference types="@angular/localize" />
+import './polyfills';
+import {provideHttpClient, withNoXsrfProtection} from '@angular/common/http';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
+import { AppModule } from "./app/app.module"
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err))
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// bootstrapApplication(NgbdPaginationBasic, {
+//   providers: [
+//     provideHttpClient(withNoXsrfProtection()),
+//   ],
+// })
+// .then(ref => {
+//   // Ensure Angular destroys itself on hot reloads.
+//   if (window['ngRef']) {
+//     window['ngRef'].destroy();
+//   }
+//   window['ngRef'] = ref;
+//
+//   // Otherwise, log the boot error
+// })
+// .catch(err => console.error(err));
